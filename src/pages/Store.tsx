beecec0a3169632d562d96beb5aa6cb952cb8eda
Shivam Sharma <meshivam81@gmail.com>
2023-06-13 16:50:09 +0530

    add new components for storeItem

--- src/pages/Store.tsx
+++ src/pages/Store.tsx
@@ -1,8 +1,19 @@
- 
+import { Col, Row } from "react-bootstrap";
+import storeItems from "../data/items.json";
+import { StoreItem } from "../components/StoreItem";
 export const Store = () => {
   return (
-    <div>Store</div>
-  )
-}
-
- 
+    <>
+      <h1 className="d-flex justify-content-center mb-2">Store</h1>
+      <Row md={2} xs={1} lg={3} className="g-3">
+        {storeItems.map((item) => {
+          return (
+            <Col key={item.id}>
+              <StoreItem {...item} />
+            </Col>
+          );
+        })}
+      </Row>
+    </>
+  );
+};
