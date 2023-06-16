a6877bae756fe48fea6fdca82633ef6f6192aaee
Shivam Sharma <meshivam81@gmail.com>
2023-06-16 14:48:31 +0530

    fix store route

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,7 +3,7 @@ import { Container } from "react-bootstrap"
 export const Home = () => {
   return (
     <> 
-    <a href="https://main--shoppingry.netlify.app/store"  className="d-flex justify-content-center mb-1"> <button type="button" className="btn btn-success ">Shop now</button></a>
+    <a href="../store"  className="d-flex justify-content-center mb-1"> <button type="button" className="btn btn-success ">Shop now</button></a>
     <Container  >
        <img src="https://cdn.pixabay.com/photo/2017/07/31/11/33/people-2557483_1280.jpg" alt="" width="100%" />
     </Container>
